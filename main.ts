d43f363d56eea756d46123418727b6141d3af34c
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-03-08 11:19:35 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,40 @@
 //% weight=100 color=#DC22E1 block="MINTspark Elecfreaks" blockId="MINTspark Elecfreaks" icon="\f0e7"
 namespace mintspark {
-    export enum DriveDirection {
-        //%block="forward"
-        Forward = 1,
-        //%block="reverse"
-        Backward = 0
+    //% weight=88
+    //% blockId=setMotorSpeed block="Set motor %motor speed to %speed\\%"
+    //% speed.min=-100 speed.max=100
+    export function setMotorSpeed(motor: neZha.MotorList, speed: number): void {
+        neZha.setMotorSpeed(motor, speed);
     }
+
+    //% weight=86
+    //% blockId=stopMotor block="Stop motor %motor"
+    export function stopMotor(motor: neZha.MotorList): void {
+        setMotorSpeed(motor, 0)
+    }
+
+    //% weight=85
+    //% blockId=stopAllMotor  block="Stop all motor"
+    export function stopAllMotor(): void {
+        setMotorSpeed(neZha.MotorList.M1, 0)
+        setMotorSpeed(neZha.MotorList.M2, 0)
+        setMotorSpeed(neZha.MotorList.M3, 0)
+        setMotorSpeed(neZha.MotorList.M4, 0)
+    }
+
+    //% weight=84
+    //% blockId=setServoAngel block="Set %servoType servo %servo angel to %angle°"
+    export function setServoAngel(servo: neZha.ServoList, angel: number): void {
+        neZha.setServoAngel(neZha.ServoTypeList._360, servo, angel);
+    }
+
+
+
+
+
+
+
+
 
     //% blockId=sonarbit block="Ultrasonic sensor %Rjpin distance %distance_unit"
     //% Rjpin.fieldEditor="gridpicker"
