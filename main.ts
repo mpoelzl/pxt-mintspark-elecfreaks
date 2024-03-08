4f538808fced62f9e924ca0f9a37cb312c8124fa
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-03-08 08:56:49 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -7,5 +7,13 @@ namespace mintspark {
         Backward = 0
     }
 
-
+    //% blockId=sonarbit block="Ultrasonic sensor %Rjpin distance %distance_unit"
+    //% Rjpin.fieldEditor="gridpicker"
+    //% Rjpin.fieldOptions.columns=2
+    //% distance_unit.fieldEditor="gridpicker"
+    //% distance_unit.fieldOptions.columns=2
+    //% subcategory=Sensor group="Digital" color=#EA5532
+    export function ultrasoundSensor(Rjpin: PlanetX_Basic.DigitalRJPin, distance_unit: PlanetX_Basic.Distance_Unit_List): number {
+        return PlanetX_Basic.ultrasoundSensor(Rjpin, distance_unit);
+    }
 }
