5611cd097eb826435c0dc2323509e5e85e785a03
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-03-08 08:52:51 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+//% weight=100 color=#DC22E1 block="MINTspark Elecfreaks" blockId="MINTspark Elecfreaks" icon="\f0e7"
+namespace mintspark {
+    export enum DriveDirection {
+        //%block="forward"
+        Forward = 1,
+        //%block="reverse"
+        Backward = 0
+    }
+
+
+}
